d64f54c05ae965dafa7dd636c9cd42177069b4ff
AnuraktaNayak <anurakta.it@gmail.com>
2025-07-09 11:56:13 -0400

    modified

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "anu-bucket" {
-  name     = "test-anu-bucket"
-  location = "US"
-  project ="bubbly-reducer-465214-m7"
+  name          = "test-anu-bucket"
+  location      = "US"
+  project       = "bubbly-reducer-465214-m7"
   force_destroy = true
 
 

16d7d670f57e47ee07f46cf21568d6914262d823
AnuraktaNayak <anurakta.it@gmail.com>
2025-07-14 17:23:45 -0400

    nhjk

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket  = "backend-gcs-bucket-statefiles" # Replace with your bucket name
-    prefix  = "terraform/state"
+    bucket = "backend-gcs-bucket-statefiles" # Replace with your bucket name
+    prefix = "terraform/state"
     # Optional prefix within the bucket
   }
 }

c967696b3ccd38d840bef43602ab0ba53a5ddd77
AnuraktaNayak <anurakta.it@gmail.com>
2025-07-14 17:21:20 -0400

    hhjjh

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket  = "backend-gcs-bucket-statefiles" # Replace with your bucket name
     prefix  = "terraform/state"
-    project = "buckettest_anuraktnayaka8123" # Optional prefix within the bucket
+    # Optional prefix within the bucket
   }
 }

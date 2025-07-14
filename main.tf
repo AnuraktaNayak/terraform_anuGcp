9f382a35430cb3b38621d7866ca6b5bda9c3c14d
AnuraktaNayak <anurakta.it@gmail.com>
2025-07-14 14:31:52 -0400

    addedkey

--- main.tf
+++ main.tf
@@ -5,5 +5,11 @@ resource "google_storage_bucket" "anu-bucket" {
   force_destroy = true
 
 
+  name: Terraform Format
+    run: terraform fmt -check
+    env:
+      GOOGLE_CREDENTIALS: ${{ secrets.GCP_SA_KEY}}
+
+
 
 }

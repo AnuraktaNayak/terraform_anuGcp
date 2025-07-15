9926cce31d0fbdb3295ffbc76f91746e80906b62
AnuraktaNayak <anurakta.it@gmail.com>
2025-07-15 12:05:52 -0400

    changesappliedtofile

--- variable.tf
+++ variable.tf
@@ -1,20 +1,20 @@
 variable "project" {
-  type = string
+  type        = string
   description = "The GCP project ID where resources will be created"
 }
 
 variable "location" {
-  type = string
+  type        = string
   description = "The location/region where the storage bucket will be created"
 }
 
 variable "bucket_name" {
-  type = string
+  type        = string
   description = "The name of the Google Cloud Storage bucket to create"
 }
 
 variable "force_destroy" {
-  type = bool
+  type        = bool
   description = "Whether to force destroy the bucket when deleting (allows deletion of non-empty buckets)"
 }
 

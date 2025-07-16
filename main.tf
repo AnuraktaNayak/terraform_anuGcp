d3cf7ec13d46bf6c3ec3a4c1f4802e8025dc8f99
AnuraktaNayak <anurakta.it@gmail.com>
2025-07-16 11:23:24 -0400

    fix change

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ resource "google_storage_bucket" "anu123-bucket" {
 
 }
 
- resource "google_sql_database_instance" "anu-cloudsql" {
-    name             = var.pg_db_name
-    database_version = var.pg_version
-    region           = var.pg_region
+resource "google_sql_database_instance" "anu-cloudsql" {
+  name             = var.pg_db_name
+  database_version = var.pg_version
+  region           = var.pg_region
 
-    settings {
-      # Second-generation instance tiers are based on the machine
-      # type. See argument reference below.
-      tier = "db-f1-micro"
-    }
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-f1-micro"
   }
+}

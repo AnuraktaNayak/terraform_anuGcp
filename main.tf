2293a50a1916a512f471b481444834bc6740efd6
AnuraktaNayak <anurakta.it@gmail.com>
2025-07-16 11:18:50 -0400

    new pg resource created

--- main.tf
+++ main.tf
@@ -6,16 +6,16 @@ resource "google_storage_bucket" "anu123-bucket" {
 
 
   resource "google_sql_database_instance" "anu-cloudsql" {
-  name             = var.pg_db_name
-  database_version = var.pg_version
-  region           = var.pg_region
+    name             = var.pg_db_name
+    database_version = var.pg_version
+    region           = var.pg_region
 
-  settings {
-    # Second-generation instance tiers are based on the machine
-    # type. See argument reference below.
-    tier = "db-f1-micro"
+    settings {
+      # Second-generation instance tiers are based on the machine
+      # type. See argument reference below.
+      tier = "db-f1-micro"
+    }
   }
-}
 
 
 }
